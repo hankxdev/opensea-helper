143e16e011ba6f96e44d910d1904549d8af736c1
hankxstudio <hank@momane.com>
2021-10-11 00:03:41 +0800

    add button and web3 to the page

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,71 @@
-import { printLine } from './modules/print';
+import $ from 'jquery';
+import { OpenSeaPort, Network } from 'opensea-js';
+import * as Web3 from 'web3';
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+const buyNowButton = `
+<button class="btn btn-primary momane-buy-now-button">
+      Buy it now
+</button>
+`;
 
-printLine("Using the 'printLine' function from the Print Module");
+let seaport = null;
+
+const config = {
+  childList: true,
+  subtree: true,
+};
+
+const targetNode = document.body;
+
+const callback = (mutationsList, observer) => {
+  for (let mutation of mutationsList) {
+    if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+      const target = mutation.target;
+      if (target && target.querySelector('.Image--image')) {
+        let container = $(target).parents('article');
+        if (container.length > 0) {
+          if (container.find('.momane-buy-now-button').length === 0) {
+            container.append(buyNowButton);
+          }
+        } else {
+          let containers = $(target).find('article');
+          if (containers.length > 0) {
+            containers.each(function () {
+              if ($(this).find('.momane-buy-now-button').length === 0) {
+                $(this).append(buyNowButton);
+              }
+            });
+          }
+        }
+      }
+    }
+  }
+};
+
+const observer = new MutationObserver(callback);
+
+observer.observe(targetNode, config);
+
+$('body').on('click', '.momane-buy-now-button', function (e) {
+  e.preventDefault();
+  const link = $(this).parent().find('a').attr('href');
+  const assetAddress = link.match(/0x\w+/)[0];
+  const assetId = link.match(/\/(\d+)/g)[1].replace('/', '');
+  if (!seaport) {
+    const provider = new Web3.providers.HttpProvider(
+      'https://mainnet.infura.io'
+    );
+    seaport = new OpenSeaPort(provider, {
+      networkName: Network.Main,
+    });
+  }
+  seaport.api
+    .getAsset({ tokenAddress: assetAddress, tokenId: assetId })
+    .then(async (asset) => {
+      const order = asset.sellOrders[0];
+      await seaport.fulfillOrder({
+        order,
+        accountAddress: this.account,
+      });
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -6,17 +6,18 @@ var webpack = require('webpack'),
   CopyWebpackPlugin = require('copy-webpack-plugin'),
   HtmlWebpackPlugin = require('html-webpack-plugin'),
   TerserPlugin = require('terser-webpack-plugin');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+
 
 const ASSET_PATH = process.env.ASSET_PATH || '/';
 
-const 
-
-var alias = {
+const alias = {
   'react-dom': '@hot-loader/react-dom',
 };
 
 // load the secrets
 var secretsPath = path.join(__dirname, 'secrets.' + env.NODE_ENV + '.js');
+const templateHtml =  path.join(__dirname, 'src', 'pages', 'template.html');
 
 var fileExtensions = [
   'jpg',
@@ -38,13 +39,10 @@ if (fileSystem.existsSync(secretsPath)) {
 var options = {
   mode: process.env.NODE_ENV || 'development',
   entry: {
-    newtab: path.join(__dirname, 'src', 'pages', 'Newtab', 'index.jsx'),
     options: path.join(__dirname, 'src', 'pages', 'Options', 'index.jsx'),
     popup: path.join(__dirname, 'src', 'pages', 'Popup', 'index.jsx'),
     background: path.join(__dirname, 'src', 'pages', 'Background', 'index.js'),
     contentScript: path.join(__dirname, 'src', 'pages', 'Content', 'index.js'),
-    devtools: path.join(__dirname, 'src', 'pages', 'Devtools', 'index.js'),
-    panel: path.join(__dirname, 'src', 'pages', 'Panel', 'index.jsx'),
   },
   chromeExtensionBoilerplate: {
     notHotReload: ['contentScript', 'devtools'],
@@ -110,6 +108,7 @@ var options = {
       .concat(['.js', '.jsx', '.ts', '.tsx', '.css']),
   },
   plugins: [
+    new NodePolyfillPlugin(),
     new webpack.ProgressPlugin(),
     // clean the build folder
     new CleanWebpackPlugin({
@@ -140,7 +139,7 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/pages/Content/content.styles.css',
+          from: 'src/pages/Content/content.css',
           to: path.join(__dirname, 'build'),
           force: true,
         },
@@ -165,33 +164,15 @@ var options = {
       ],
     }),
     new HtmlWebpackPlugin({
-
-      filename: 'newtab.html',
-      chunks: ['newtab'],
-      cache: false,
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'pages', 'Options', 'index.html'),
+      template: templateHtml,
       filename: 'options.html',
       chunks: ['options'],
       cache: false,
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'pages', 'Popup', 'index.html'),
+      template: templateHtml,
       filename: 'popup.html',
       chunks: ['popup'],
-      cache: false,
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'pages', 'Devtools', 'index.html'),
-      filename: 'devtools.html',
-      chunks: ['devtools'],
-      cache: false,
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'pages', 'Panel', 'index.html'),
-      filename: 'panel.html',
-      chunks: ['panel'],
       cache: false,
     }),
   ],
